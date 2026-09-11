677280f61f3c981be8a0241c65368a34d41fd16e
mariana-sesc <liv.s.n00@gmail.com>
2026-09-11 10:34:02 -0300

    Create tipos-de-dados-arrays.js

---
+++ tipos-de-dados-arrays.js
@@ -0,0 +1,6 @@
+let frutas = ["maçã" , "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5]; // Array de números
+let misto = [true, "texto", 42]; // Array com diferentes tipos de dados
+console.log("Frutas:", frutas);
+console.log("Números:", numeros);
+console.log("Misto:", misto);
