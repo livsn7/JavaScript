4d8931eb4231fea8b4dc360da97db22e3900a395
mariana-sesc <liv.s.n00@gmail.com>
2026-09-11 10:53:03 -0300

    Create tipos-de-dados-objects.js

---
+++ tipos-de-dados-objects.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "carlos",
+    idade: 28,
+    casado: true,
+};
+// Acessando propriedades do objeto
+console.log(pessoa.nome); // "Carlos"
+console.log(pessoa.idade); // 28
